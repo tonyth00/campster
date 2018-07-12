fb1b79923e7667a7e3dc93199603969feb1cb7b6
Tony Huang <tonyth00@gmail.com>
2018-07-12 00:55:27 -0700

    screen size
    fix
    
    update

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.post("/register", function(req, res) {
 		} else {
 			//If registered successfully, authenticate user and create session
 			passport.authenticate("local")(req, res, function() {
-				req.flash("success", "Welcome to YelpCamp, " + user.username + "!");
+				req.flash("success", "Welcome to Campster, " + user.username + "!");
 				res.redirect("/campgrounds");
 			});
 		}
